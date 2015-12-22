a06f5283c14e63699ddc85f55d96b78a2c13764d
Yan-Fa Li <yanfali@gmail.com>
2015-12-21 23:43:51 -0800

    Circle Area

--- src/Main.purs
+++ src/Main.purs
@@ -4,9 +4,12 @@ import Prelude
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
 import Math (sqrt)
+import Math (pi)
 
 diagonal w h = sqrt (w * w + h * h)
+circleArea r = pi * r * r
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log "Hello sailor!"
   print (diagonal 3.0 4.0)
+  print (circleArea 3.0)
