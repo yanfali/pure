23d1f0d271f57caf3e76ed183a5ffbba9836d9c4
Yan-Fa Li <yanfali@gmail.com>
2015-12-21 23:33:49 -0800

    Start

---
+++ src/Main.purs
@@ -0,0 +1,11 @@
+module Main where
+
+import Prelude
+import Control.Monad.Eff
+import Control.Monad.Eff.Console
+import Math (sqrt)
+
+diagonal w h = sqrt (w * w + h * h)
+main :: forall e. Eff (console :: CONSOLE | e) Unit
+main = do
+  log "Hello sailor!"
