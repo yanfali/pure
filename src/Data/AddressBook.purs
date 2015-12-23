79fb9a8cf2f28958d184961f7d032a2a0ab51d5b
Yan-Fa Li <yanfali@gmail.com>
2015-12-22 22:32:53 -0800

    chp 3.6 - 3.9

--- src/Data/AddressBook.purs
+++ src/Data/AddressBook.purs
@@ -6,3 +6,27 @@ import Data.List
 import Data.Maybe
 
 import Control.Plus (empty)
+
+type Entry =
+  { firstName :: String
+  , lastName  :: String
+  , address   :: Address
+  }
+
+type Address =
+  { street :: String
+  , city   :: String
+  , state  :: String
+  }
+
+type AddressBook = List Entry
+
+showEntry :: Entry -> String
+showEntry entry = entry.lastName ++ ", " ++
+                  entry.firstName ++ ": " ++
+                  showAddress entry.address
+
+showAddress :: Address -> String
+showAddress addr = addr.street ++ ", " ++
+                   addr.city ++ ", " ++
+                   addr.state
